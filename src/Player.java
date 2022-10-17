3d3eb6285e10f6e2be2038cd1200246a4fab46c9
A-Jama01 <113302684+A-Jama01@users.noreply.github.com>
2022-10-16 21:13:15 -0400

    Add Player class file

---
+++ src/Player.java
@@ -0,0 +1,12 @@
+public class Player {
+    private String name;
+    private int score;
+    private String[] tiles;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+        this.tiles = new String[7];
+    }
+
+}
