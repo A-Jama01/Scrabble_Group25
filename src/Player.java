522eab66dad49b0063c31c0285bf06c9e148f296
A-Jama01 <113302684+A-Jama01@users.noreply.github.com>
2022-10-19 16:59:31 -0400

    Add Player class

--- src/Player.java
+++ src/Player.java
@@ -1,12 +1,37 @@
+import java.util.*;
+
 public class Player {
     private String name;
     private int score;
-    private String[] tiles;
+    private ArrayList<String> rack;
 
     public Player(String name) {
         this.name = name;
         this.score = 0;
-        this.tiles = new String[7];
+        this.rack = new ArrayList<>();
+    }
+
+    /**
+     * Add to player score
+     */
+    public void addPoints(int points) {
+        this.score += points;
+    }
+
+    /**
+     * Add tile to player's rack
+     * @param tile  Tile to be added
+     */
+    public void addTiles(String tile) {
+        this.rack.add(tile);
+    }
+
+    /**
+     * Return the number of tiles in the players rack
+     * @return int representing rack size
+     */
+    public int rackSize() {
+        return this.rack.size();
     }
 
 }
