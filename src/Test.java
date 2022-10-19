06c0d97c0f34383a0f9989cc746e2c3d12d9620f
Monishkumar Sivakumar <monish.sivakumar@cmail.carleton.ca>
2022-10-19 16:21:28 -0400

    Created Word Class
    Now the Word Class has Numbers assigned to each Letter. The Word class also calculates the score of the word inputted into it.

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+//Monishkumar Sivakumar - 101115115
+public class Test {
+    public static void main(String[] args) {
+        String word = "BYEBYE";
+        System.out.println("'" + word + "' is worth " + Word.score(word) + " points");
+    }
+}
