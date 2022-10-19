fd3e1436f4d10e01c69fd7e1d157aa827e57c782
Monishkumar Sivakumar <monish.sivakumar@cmail.carleton.ca>
2022-10-19 18:15:50 -0400

    Created Dictionary Class

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,11 +1,28 @@
-package File;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Dictionary {
 
+    public static void main(String[] args)
+    {
+        try {
+            File dictionary = new File("Dictionary.txt");
+            Scanner words = new Scanner(dictionary);
+            String word = "able";
+            while (words.hasNextLine()) {
+                String data = words.nextLine();
+                String[] check = data.split(" ");
+                for (String s: check){
+                    if (s.equals(word)){
+                        System.out.println("Word is Legal");
+                    }
+                }
+            }
+            words.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
