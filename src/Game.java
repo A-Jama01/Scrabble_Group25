5792eef86e07826757ce2d1ec9894f406b7f2626
A-Jama01 <113302684+A-Jama01@users.noreply.github.com>
2022-10-21 17:40:28 -0400

    Remove changes to main branch

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,5 @@
 import java.util.*;
 
 public class Game {
-    private Parser parser;
-    private ScrabbleDictionary dict;
-    private Board board;
-    private Bag bag;
-    private ArrayList<Player> players;
+
 }
