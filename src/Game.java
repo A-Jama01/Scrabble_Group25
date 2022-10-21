903455bb8ad64d517c5631fe22a51317c7576ccb
A-Jama01 <113302684+A-Jama01@users.noreply.github.com>
2022-10-21 17:49:27 -0400

    Add Fields to game class

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,9 @@
-public class Game {
+import java.util.*;
 
+public class Game {
+    private Parser parser;
+    private ScrabbleDictionary dict;
+    private Bag bag;
+    private Board board;
+    private ArrayList<Player> players;
 }
