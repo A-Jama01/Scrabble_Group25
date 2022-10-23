1437cce29e8930467817886769f187755da13a69
Monishkumar Sivakumar <monish.sivakumar@cmail.carleton.ca>
2022-10-23 15:08:46 -0400

    Initialized the letters in a seperate class

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,9 @@
 //Monishkumar Sivakumar - 101115115
+//Test class to check whether the Word Class works
 public class Test {
     public static void main(String[] args) {
         String word = "BYEBYE";
-        System.out.println("'" + word + "' is worth " + Word.score(word) + " points");
+        Word words = new Word();
+        System.out.println("'" + word + "' is worth " + words.score(word) + " points");
     }
 }
--- src/Word.java
+++ src/Word.java
@@ -3,40 +3,44 @@ import java.util.HashMap;
 
 public class Word {
 
-    static HashMap<String, Integer> letters = new HashMap<String, Integer>();;
+    private HashMap<String, Integer> letters = new HashMap<String, Integer>();
 
-    public static int score(String word) {
-        letters.put("A", 1);
-        letters.put("B", 3);
-        letters.put("C", 3);
-        letters.put("D", 2);
-        letters.put("E", 1);
-        letters.put("F", 4);
-        letters.put("G", 2);
-        letters.put("H", 4);
-        letters.put("I", 1);
-        letters.put("J", 8);
-        letters.put("K", 5);
-        letters.put("L", 1);
-        letters.put("M", 3);
-        letters.put("N", 1);
-        letters.put("O", 1);
-        letters.put("P", 3);
-        letters.put("Q", 10);
-        letters.put("R", 1);
-        letters.put("S", 1);
-        letters.put("T", 1);
-        letters.put("U", 1);
-        letters.put("V", 4);
-        letters.put("W", 4);
-        letters.put("X", 8);
-        letters.put("Y", 4);
-        letters.put("Z", 10);
+    //This class initializes the values for the Scrabble Letters
+    public Word(){
+        this.letters.put("A", 1);
+        this.letters.put("B", 3);
+        this.letters.put("C", 3);
+        this.letters.put("D", 2);
+        this.letters.put("E", 1);
+        this.letters.put("F", 4);
+        this.letters.put("G", 2);
+        this.letters.put("H", 4);
+        this.letters.put("I", 1);
+        this.letters.put("J", 8);
+        this.letters.put("K", 5);
+        this.letters.put("L", 1);
+        this.letters.put("M", 3);
+        this.letters.put("N", 1);
+        this.letters.put("O", 1);
+        this.letters.put("P", 3);
+        this.letters.put("Q", 10);
+        this.letters.put("R", 1);
+        this.letters.put("S", 1);
+        this.letters.put("T", 1);
+        this.letters.put("U", 1);
+        this.letters.put("V", 4);
+        this.letters.put("W", 4);
+        this.letters.put("X", 8);
+        this.letters.put("Y", 4);
+        this.letters.put("Z", 10);
+    }
 
+    //This class will check the score of the word inputted
+    public int score(String word) {
         int sum = 0;
         String[] letter = word.split("");
         for(int i = 0; i < word.length();i++) {
-            sum += letters.get(letter[i]);
+            sum += this.letters.get(letter[i]);
         }
         return sum;
 
