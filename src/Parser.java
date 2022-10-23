f63e47e7a48c1e919fef0641b5b9fd9c092ae931
Christina <christinaxdang@gmail.com>
2022-10-23 16:22:58 -0400

    Parser class

--- src/Parser.java
+++ src/Parser.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * The Parser class to get inputs
+ *
+ * @author Christina Dang 101141609
+ */
+
 public class Parser {
+    private String line;
+
+    /**
+     * A method to get the input of the user
+     *
+     * @return line string, the input
+     */
+    public String getInput(){
+        Scanner scan = new Scanner(System.in);
+        line = scan.nextLine();
+        return line;
+    }
 }
