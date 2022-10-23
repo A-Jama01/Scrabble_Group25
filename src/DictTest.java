853f7809417af58c757bb6f1f7547ddd4b73e4eb
Monishkumar Sivakumar <monish.sivakumar@cmail.carleton.ca>
2022-10-23 14:57:23 -0400

    Added Comments to the files

--- src/DictTest.java
+++ src/DictTest.java
@@ -1,7 +1,9 @@
+//Monishkumar Sivakumar - 1011115115
+//Test class to test values for the Dictionary Class
 public class DictTest {
 
     public static void main(String[] args) {
-        String word = "able";
+        String word = "ABLE";
         System.out.println(Dictionary.check(word));
     }
 
