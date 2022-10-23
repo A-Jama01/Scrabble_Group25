7372d5354b8a410ff0191df6c22cbacb2e541bd7
monishkumar-sivakumar <63323924+monishkumar-sivakumar@users.noreply.github.com>
2022-10-23 16:14:57 -0400

    Delete Test.java
    not needed in main

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-//Monishkumar Sivakumar - 101115115
-//Test class to check whether the Word Class works
-public class Test {
-    public static void main(String[] args) {
-        String word = "BYEBYE";
-        Word words = new Word();
-        System.out.println("'" + word + "' is worth " + words.score(word) + " points");
-    }
-}
