bad6876caf286bc2486e39db388cf3543a2d334b
Henry <henrylin4@cmail.carleton.ca>
2022-11-13 16:02:16 -0500

    added a getter for Board, mostly complete implementation of BoardView

--- src/BoardView.java
+++ src/BoardView.java
@@ -10,9 +10,22 @@ public class BoardView extends JFrame {
 
     private JButton[][] buttons;
 
+    /**
+     * Floating tiles are letters that a player
+     * is attempting to place on the board. They
+     * are displayed visually on the BoardView,
+     * but do not affect the actual state of the Board.
+     */
     private ArrayList<JButton> floating;
+    private int floatingCol;
+    private int floatingRow;
+    private int floatingDir;
 
-    public BoardView() {  // add the controller as a parameter
+    private Board board;
+
+    public BoardView(Board board) {  // add the controller as a parameter
+        this.board = board;
+        floating = new ArrayList<>();
         setLayout(new GridLayout(Board.WIDTH + 1, Board.HEIGHT + 1));
 
         buttons = new JButton[Board.WIDTH][Board.HEIGHT];
@@ -27,18 +40,131 @@ public class BoardView extends JFrame {
             label.setHorizontalAlignment(JLabel.CENTER);
             add(label);
             for (int col = 0; col < Board.HEIGHT; col++) {
-                JButton tile = new JButton(Integer.toString(col) + "," + Integer.toString(row));
+                JButton tile = new JButton();
                 tile.setActionCommand("try " + Board.column.values()[col].toString() + col);
+                // JUNK CODE _
+                int finalCol = col;
+                int finalRow = row;
+                tile.addActionListener(e -> setFloatingTile(finalCol, finalRow, "Z"));
+                // JUNK CODE ^
                 buttons[col][row] = tile;
                 add(tile);
             }
         }
     }
 
+    /**
+     * Update the BoardView to reflect the current state of
+     * the Board, removing any floating tiles. Disable
+     * buttons for placed letters.
+     */
+    public void refresh() {
+        for (int col = 0; col < Board.WIDTH; col++) {
+            for (int row = 0; row < Board.HEIGHT; row++) {
+                buttons[col][row].setText(board.letterAt(col, row));
+                buttons[col][row].setEnabled(board.letterAt(col, row).equals(Board.EMPTY));
+            }
+        }
+        floating.clear();
+    }
+
+    /**
+     * Place a floating letter on the board. If there is
+     * already a floating letter there, returns that letter
+     * and replaces it with the given letter.
+     * @param col    The column on the grid
+     * @param row    The row on the grid
+     * @param letter The letter to place
+     * @return A letter that was swapped out, or null if none
+     */
+    public String setFloatingTile(int col, int row, String letter) {
+        // JUNK CODE _
+        if (col == 14 && row == 14) {
+            System.out.println(getFloatingWord());
+            return null;
+        }
+        // JUNK CODE ^
+        switch (floating.size()) {
+            case 0:
+                floatingCol = col;
+                floatingRow = row;
+                break;
+            case 1:
+                if (col == floatingCol) {
+                    floatingDir = Board.VERTICAL;
+                } else if (row == floatingRow) {
+                    floatingDir = Board.HORIZONTAL;
+                } else { return null; }
+                break;
+            default:
+                if (floatingDir == Board.VERTICAL && col != floatingCol) {
+                    return null;
+                } else if (floatingDir == Board.HORIZONTAL && row != floatingRow) {
+                    return null;
+                }
+        }
+        JButton target = buttons[col][row];
+        floating.add(target);
+        if (target.getText().equals(Board.EMPTY)) {
+            target.setText(letter);
+            return null;
+        } else {
+            String swapped = target.getText();
+            target.setText(letter);
+            return swapped;
+        }
+    }
+
+    /**
+     * Return the main word created by the floating tiles and
+     * existing tiles currently on the BoardView.
+     * Returns null if there are no floating tiles or if there
+     * are disconnected floating tiles.
+     * @return The word created by floating tiles, or null if no word
+     */
+    public String getFloatingWord() {
+        if (floating.size() == 0) { return null; }
+
+        StringBuilder word = new StringBuilder(Math.max(Board.WIDTH, Board.HEIGHT));
+        int lettersUsed = 0;
+        int upperLimit = (floatingDir == Board.HORIZONTAL)? Board.WIDTH : Board.HEIGHT;
+        int startingLetterIndex = (floatingDir == Board.HORIZONTAL)? floatingCol : floatingRow;
+        for (int index = startingLetterIndex - 1; 0 <= index; index--) {
+            JButton tile = (floatingDir == Board.HORIZONTAL)?
+                    buttons[index][floatingRow] : buttons[floatingCol][index];
+            if (tile.getText().equals(Board.EMPTY)) {
+                break;
+            } else if (floating.contains(tile)) {
+                lettersUsed++;
+            }
+            word.insert(0, tile.getText());
+        }
+        for (int index = startingLetterIndex; index < upperLimit; index++) {
+            JButton tile = (floatingDir == Board.HORIZONTAL)?
+                    buttons[index][floatingRow] : buttons[floatingCol][index];
+            if (tile.getText().equals(Board.EMPTY)) {
+                break;
+            } else if (floating.contains(tile)) {
+                lettersUsed++;
+            }
+            word.append(tile.getText());
+        }
+        if (lettersUsed == floating.size()) {
+            return word.toString();
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
-        BoardView bv = new BoardView();
+        Board b = new Board();
+
+        BoardView bv = new BoardView(b);
         bv.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        bv.setSize(500, 500);
+        bv.setSize(750, 750);
         bv.setVisible(true);
+
+        b.place("HELLO", "H8");
+        b.place("WORLD", "12G");
+        bv.refresh();
     }
 }
