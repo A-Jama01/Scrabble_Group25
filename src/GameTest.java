910e0210b7f2de00f7b46c7427f14d9381fc2320
Monishkumar Sivakumar <monish.sivakumar@cmail.carleton.ca>
2022-11-13 23:25:29 -0500

    Created GameTest

---
+++ src/GameTest.java
@@ -0,0 +1,21 @@
+/** GameTest Class - The class tests the Game Class
+ *
+ * @author Monishkumar Sivakumar 101115115
+ **/
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class GameTest {
+    private Game game;
+
+    @Before
+    public void setUp(){
+        game = new Game();
+    }
+
+    @Test
+    public void testPlaceWord(){
+        fail("The placement of words onto the BoardView/GameView has not been implemented yet");
+    }
+}
