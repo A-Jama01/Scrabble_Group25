93ffc3b0d1f28a538eaa117ec74bf27edb16542f
Monishkumar Sivakumar <monish.sivakumar@cmail.carleton.ca>
2022-12-09 21:38:01 -0500

    Added Undo/Redo

---
+++ src/SaveMoves.java
@@ -0,0 +1,39 @@
+/** Save Class - This class saves the scrabble game using the date and time
+ *
+ * @author Monishkumar Sivakumar 101115115
+ **/
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.nio.file.Files;
+
+public class SaveMoves {
+
+    ScrabbleController game;
+    //private int num;
+
+    public SaveMoves(ScrabbleController _game){
+        this.game = _game;
+    }
+
+    public void save(int num) {
+        System.err.println("SaveMove " + num);
+
+        String file_name = "states/scrabble_" + num + ".bin";
+        try{
+            new File("states").mkdir();
+        }
+        catch (Exception ex){
+            ex.printStackTrace(System.err);
+        }
+
+        try{
+            Files.write(new File(file_name).toPath(), Serialization.write_base64(game.getGame()).getBytes());
+        }
+        catch (Exception ex){
+            ex.printStackTrace(System.err);
+        }
+
+        Serialization.write_base64(game.getGame());
+    }
+}
